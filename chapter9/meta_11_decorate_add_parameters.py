54d8175eaa021d889b3821498c751656db0dcea6
zlxwl <lei.zhong@beibei.com>
2022-04-18 19:05:01 +0800

    chapter9 11

---
+++ chapter9/meta_11_decorate_add_parameters.py
@@ -0,0 +1,59 @@
+from functools import wraps
+import inspect
+
+
+# def optional_debug(func):
+#     @wraps(func)
+#     def wrapper(*args, debug=False, **kwargs):
+#         if debug:
+#             print('calling', func.__name__)
+#         return func(*args, **kwargs)
+#     return wrapper
+#
+#
+# @optional_debug
+# def spam(a, b, c):
+#     print(a, b, c)
+def optional_debug(func):
+    if 'debug' in inspect.getfullargspec(func).args:
+        raise TypeError('debug argument already defined!')
+
+    @wraps(func)
+    def wrapper(*args, debug=False, **kwargs):
+        if debug:
+            print('calling {}'.format(func.__name__))
+        return func(*args, kwargs)
+    parameters = list(inspect.signature(func).parameters.values())
+    parameters.append(
+        inspect.Parameter('debug',
+                          inspect.Parameter.KEYWORD_ONLY,
+                          default=False)
+    )
+    wrapper.__signature__ = inspect.signature(func).replace(parameters=parameters)
+    return wrapper
+
+
+@optional_debug
+def a(x):
+    pass
+
+
+@optional_debug
+def b(x, y, z):
+    pass
+
+
+@optional_debug
+def c(x, y):
+    pass
+
+
+@optional_debug
+def add(x, y):
+    return x + y
+
+
+if __name__ == '__main__':
+    # spam(1, 2, 3)
+    # spam(1, 2, 3, debug=True)
+    print(inspect.signature(add))
