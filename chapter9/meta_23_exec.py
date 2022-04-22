f598bbe7eb2b5c9ab93928ef027d6d5358886516
zlxwl <625015751@qq.com>
2022-04-22 22:45:24 +0800

    chapter 23

--- chapter9/meta_23_exec.py
+++ chapter9/meta_23_exec.py
@@ -5,8 +5,33 @@ def exec_demo():
     return loc['b']
 
 
-if __name__ == '__main__':
-    b = exec_demo()
+def exec_demo_2():
+    x = 0
+    exec('x+=1')
+    print(x)
+
+
+def exec_demo_3():
+    x = 0
+    loc = locals()
+    print(loc)
+    exec('x+=1')
+    print(loc)
+    locals()
+    print(loc)
+
+
+def exec_demo_4():
+    a = 13
+    loc = {'a': a}
+    glb = {}
+    exec('b = a + 1', glb, loc)
+    b = loc['b']
     print(b)
 
 
+if __name__ == '__main__':
+    # b = exec_demo()
+    # exec_demo_2()
+    # exec_demo_3()
+    exec_demo_4()
